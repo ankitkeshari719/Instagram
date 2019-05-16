6b910b91634743a267331f72e1a0d76e31956794
Ankit Keshari <ankit.keshari@gslab.com>
2019-05-16 17:02:07 +0530

    [Instagram clone]: CSS Fixes

--- src/components/User/UserPost/UserPost.js
+++ src/components/User/UserPost/UserPost.js
@@ -13,30 +13,28 @@ function UserPost(props) {
   return (
     <div>
       <main>
-        <div className={classes.containe}>
-          <div className={classes.gallery}>
-            <div className={classes.gallery_item} tabIndex="0">
-              <img
-                src={image}
-                className={classes.gallery_image}
-                alt="gallery_image"
-              />
-              <div className={classes.gallery_item_type}>
-                <span className={classes.visually_hidden}>Video</span>
-                <FontAwesomeIcon icon={type === "vedio" ? faVideo : faClone} />
-              </div>
-              <div className={classes.gallery_item_info}>
-                <ul>
-                  <li className={classes.gallery_item_likes}>
-                    <span className={classes.visually_hidden}>Likes:</span>
-                    <FontAwesomeIcon icon={faHeart} /> {noOfLikes}
-                  </li>
-                  <li className={classes.gallery_item_comments}>
-                    <span className={classes.visually_hidden}>Comments:</span>
-                    <FontAwesomeIcon icon={faComment} /> {noOfComments}
-                  </li>
-                </ul>
-              </div>
+        <div className={classes.gallery}>
+          <div className={classes.gallery_item} tabIndex="0">
+            <img
+              src={image}
+              className={classes.gallery_image}
+              alt="gallery_image"
+            />
+            <div className={classes.gallery_item_type}>
+              <span className={classes.visually_hidden}>Video</span>
+              <FontAwesomeIcon icon={type === "vedio" ? faVideo : faClone} />
+            </div>
+            <div className={classes.gallery_item_info}>
+              <ul>
+                <li className={classes.gallery_item_likes}>
+                  <span className={classes.visually_hidden}>Likes:</span>
+                  <FontAwesomeIcon icon={faHeart} /> {noOfLikes}
+                </li>
+                <li className={classes.gallery_item_comments}>
+                  <span className={classes.visually_hidden}>Comments:</span>
+                  <FontAwesomeIcon icon={faComment} /> {noOfComments}
+                </li>
+              </ul>
             </div>
           </div>
         </div>
